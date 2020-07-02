ddd03a05f1a206700e8fc1188033ab911e0ccd1b
david <daviddecastro42@live.co.uk>
2020-07-03 00:46:05 +0100

    finished navigation component and finished css responsive navigation menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Navigation from './components/navigation';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import PageRenderer from './page-renderer';
 
 function App() {
-  return <div className='App'></div>;
+  const user = {
+    firstName: 'David',
+    lastName: 'De Castro'
+  };
+
+  return (
+    <Router>
+      <div className='App'>
+        <Navigation user={user} />
+        <Switch>
+          <Route path='/:page' component={PageRenderer} />
+          <Route path='/' render={() => <Redirect to='/home' />} />
+          <Route component={() => 404} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import '@quasar/extras/ionicons-v4/ionicons-v4.css';
+
+import './assets/scss/base.scss';
 
 ReactDOM.render(
   <React.StrictMode>
